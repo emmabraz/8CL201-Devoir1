3655d945662b7dd884f65c8565970faeaf8f3982
emmabraz <ebdacostar@etu.uqac.ca>
2024-11-11 14:56:08 -0500

    delete modif

--- Devoir1_/deleteallazure.ps1
+++ Devoir1_/deleteallazure.ps1
@@ -1,6 +1,6 @@
 ﻿# Lister et supprimer les ressources 
-az resource list --subscription 0fcbc58f-03c0-4daa-9470-840cff312d29 --query "[].id" -o tsv 
-$resources = az resource list --subscription 0fcbc58f-03c0-4daa-9470-840cff312d29 --query "[].id" -o tsv
+az resource list --subscription 7e5b65cd-d857-44e9-a7c8-f425a082239f --query "[].id" -o tsv 
+$resources = az resource list --subscription 7e5b65cd-d857-44e9-a7c8-f425a082239f --query "[].id" -o tsv
 foreach ($resource in $resources)
 { 
   write-host $resource 
